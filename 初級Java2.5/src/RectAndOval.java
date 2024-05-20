a8e30a831dd4e3cd1cdc6bf173d3c78c248ca2a1
riku0817 <inbederacade@gmail.com>
2024-05-20 10:36:51 +0900

    確認問題3-1 再度提出

---
+++ 初級Java2.5/src/RectAndOval.java
@@ -0,0 +1,9 @@
+
+public class RectAndOval extends MyFrame {
+	public void run() {
+		fillRect(85,105,35,30);
+		fillRect(70,120,65,20);
+		fillOval(77,140,18,18);
+		fillOval(110,140,18,18);
+	}
+}
