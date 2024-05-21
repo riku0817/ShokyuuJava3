b0a67cd48ab0f339bd3f18027534936ee7d9f7ec
riku0817 <inbederacade@gmail.com>
2024-05-21 11:10:12 +0900

    追加問題2 提出

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -2,13 +2,22 @@
 public class RectIf extends MyFrame {
 	public void run() {
 		int x = 30;
-		int y = 10;
+		int y = 20;
+		int z= 250;
 		setColor(0,128,0);
-		for(int i = 0; i < 9;i++) {
-			fillRect(x,100,10,y);
+		for(int i = 0; i < 10;i++) {
+			fillRect(x,100,10,200);
 			x += 20;
-			y += 20;
+			
+			
 		}
+		setColor(255,255,255);
+		for(int i = 0; i < 10;i++) {
+			fillRect(y,z,30,30);
+			y += 20;
+			z -= 10;
+		}
+		
 	}
 
 }
