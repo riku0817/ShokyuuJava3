d67ffa4ceea9223cedc8dde93a96406d1f9d1af5
riku0817 <inbederacade@gmail.com>
2024-05-27 10:53:02 +0900

    練習問題5-2 提出

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -4,22 +4,25 @@ public class Animation extends MyFrame {
 		setColor(0, 128, 0);
 		int y = 100;
 		int x = 30;
-		System.out.println("初回の座標値  " + x + " " + y);
-		while (true) {
-			while (x <= 170) {
-				clear();
-				fillRect(x, y, 10, 100);
-				x += 5;
-				System.out.println("1つ目のループ " + x + " " + y);
-				sleep(0.1);
-			}
-			while (x >= 30) {
-				clear();
-				fillRect(x, y, 10, 100);
-				x -= 5;
-				System.out.println("2つ目のループ " + x + " " + y);
-				sleep(0.1);
-			}
+		while (x <= 170) {
+			clear();
+			fillRect(x, y, 10, 100);
+			x += 5;
+			y += 5;
+			sleep(0.1);
+		}
+		while (x >= 30) {
+			clear();
+			fillRect(x, y, 10, 100);
+			x -= 5;
+			sleep(0.1);
+		}
+		while (y >= 60) {
+			clear();
+			fillRect(x, y, 10, 100);
+			x += 5;
+			y -= 5;
+			sleep(0.1);
 		}
 
 	}
