ef1e2b49e2c991821c5baedc4d813172c3e9b0e7
riku0817 <inbederacade@gmail.com>
2024-05-27 11:42:46 +0900

    追加問題3 提出

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,21 +2,36 @@
 public class Animation extends MyFrame {
 	public void run() {
 		setColor(0, 128, 0);
-		int y = 100;
+		int y = 50;
+		int ylocation = y;
 		int x = 30;
-		for (int i = 0; i < 3; i++) {
-			while (x <= 170) {
+		while (true) {
+			while (x <= 350) {
 				clear();
 				fillRect(x, y, 10, 100);
 				x += 5;
 				sleep(0.03);
 			}
+			while (y <= ylocation + 50) {
+				clear();
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.03);
+			}
+			ylocation = y;
 			while (x >= 30) {
 				clear();
 				fillRect(x, y, 10, 100);
 				x -= 5;
 				sleep(0.03);
 			}
+			while (y <= ylocation + 50) {
+				clear();
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.03);
+			}
+			ylocation = y;
 		}
 
 	}
