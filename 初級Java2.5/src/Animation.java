ec447a39503fedb149a580342adb57362ca34de9
riku0817 <inbederacade@gmail.com>
2024-05-27 10:32:16 +0900

    アニメーション変更

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -2,11 +2,21 @@
 public class Animation extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
+		int y = 100;
 		int x = 30;
-		while(x<=170) {
+		System.out.println(x+" "+y);
+		while(x <= 170) {
 			clear();
-			fillRect(x,80,10,100);
-			x+=5;
+			fillRect(x,y,10,100);
+			x += 5;
+			System.out.println(x+" "+y);
+			sleep(0.1);
+		}
+		while(x >= 30) {
+			clear();
+			fillRect(x,y,10,100);
+			x -= 5;
+			System.out.println(x+" "+y);
 			sleep(0.1);
 		}
 	}
