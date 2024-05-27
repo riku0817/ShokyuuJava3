b5d4bbe8117d7659dedeec49dc17eb94c443621a
riku0817 <inbederacade@gmail.com>
2024-05-27 10:47:45 +0900

    図形を往復させる

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,23 +1,26 @@
 
-public class Animation extends MyFrame{
+public class Animation extends MyFrame {
 	public void run() {
-		setColor(0,128,0);
+		setColor(0, 128, 0);
 		int y = 100;
 		int x = 30;
-		System.out.println(x+" "+y);
-		while(x <= 170) {
-			clear();
-			fillRect(x,y,10,100);
-			x += 5;
-			System.out.println(x+" "+y);
-			sleep(0.1);
+		System.out.println("初回の座標値  " + x + " " + y);
+		while (true) {
+			while (x <= 170) {
+				clear();
+				fillRect(x, y, 10, 100);
+				x += 5;
+				System.out.println("1つ目のループ " + x + " " + y);
+				sleep(0.1);
+			}
+			while (x >= 30) {
+				clear();
+				fillRect(x, y, 10, 100);
+				x -= 5;
+				System.out.println("2つ目のループ " + x + " " + y);
+				sleep(0.1);
+			}
 		}
-		while(x >= 30) {
-			clear();
-			fillRect(x,y,10,100);
-			x -= 5;
-			System.out.println(x+" "+y);
-			sleep(0.1);
-		}
+
 	}
 }
