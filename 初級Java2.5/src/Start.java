e5eff7cc813af2e9d1d9f74b2536d5f7f0d7eda8
riku0817 <inbederacade@gmail.com>
2024-05-28 09:59:06 +0900

    顔をクラス化

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プログラムを実行します");
-		MyFrame frame1=new Animation();
+		MyFrame frame1=new DisplayFaceObject();
 	}
 
 }
