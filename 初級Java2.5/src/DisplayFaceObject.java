204e8042d9023840069064150ea232eee69b83d5
riku0817 <inbederacade@gmail.com>
2024-06-03 09:38:36 +0900

    練習問題7-1 セーブ

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,12 +1,8 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face[] faces = new Face[5];
+		Face[] faces = new Face[1];
 		faces[0] = new Face(50, 50, 10, 5);
-		faces[1] = new Face(200, 100, -10, -5);
-		faces[2] = new Face(0, 100, 5, 0);
-		faces[3] = new Face(200, 100, -5, 0);
-		faces[4] = new Face(100, 0, 0, 5);
 		for (int i = 0; i < 30; i++) {
 			clear();
 			for (int j = 0; j < faces.length; j++) {
