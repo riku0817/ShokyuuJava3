b8c248100102265a3da20a457122d18b2c95eaa2
riku0817 <inbederacade@gmail.com>
2024-06-10 10:11:50 +0900

    リスト8.6

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -2,7 +2,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("プログラムを実行します");
-		MyFrame f = new DisplayFaceObject();
+		MyFrame f = new DisplayCarTrain();
 	}
 
 }
