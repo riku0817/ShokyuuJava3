122938aa981c1994c07756b2b63d394b7d0aa341
riku0817 <inbederacade@gmail.com>
2024-06-11 09:52:07 +0900

    練習問題9-1 提出

--- 初級Java2.5/src/Car.java
+++ 初級Java2.5/src/Car.java
@@ -31,13 +31,30 @@ public class Car extends Vehicle implements KeyListener {
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			vx = 5;
 		}
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			vy = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			vy = 5;
+		}
 	}
 
 	// キーが離されたとき
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		vx = 0;
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			vy = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			vy = 0;
+		}
 	}
 
 }
