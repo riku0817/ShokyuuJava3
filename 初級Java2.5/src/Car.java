8ac58415dd3c6a2cb9229690e96896c22fbff7a2
riku0817 <inbederacade@gmail.com>
2024-06-11 09:49:18 +0900

    リスト9.11

--- 初級Java2.5/src/Car.java
+++ 初級Java2.5/src/Car.java
@@ -1,11 +1,10 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Car extends Vehicle implements KeyListener{
-
+public class Car extends Vehicle implements KeyListener {
 
 	public Car(int x, int y, int vx, int vy) {
-		super(x,y,vx,vy);
+		super(x, y, vx, vy);
 	}
 
 	public void draw(MyFrame frame) {
@@ -14,24 +13,31 @@ public class Car extends Vehicle implements KeyListener{
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
 	}
+
 	// キーから文字が入力されたとき
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
+
 	// キーが押されたとき
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("キーが押されました");
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 5;
+		}
 	}
+
 	// キーが離されたとき
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		vx = 0;
 	}
-    
-	
+
 }
